0d9d5137cc092e9616dfd6d85bb7ed2a2ffa1f40
Shaun Stone <shaunmstone@gmail.com>
2017-12-25 12:15:36 +0000

    fix deploy

--- src/helpers/deploy-game.js
+++ src/helpers/deploy-game.js
@@ -1,12 +1,19 @@
 const { cd, exec } = require('shelljs');
 const { deploy: { baseBranch } } = require('./../../config');
 const deployPath = require('./get-deploy-path');
+const log = require('./log');
+const { INFO } = require('./../constants/log-level');
 
 const deployGame = (branchName, projectName, ticketId) => {
+  log(`changing to path ${deployPath}`, INFO);
   cd(deployPath);
+  log(`staging project ${projectName}`, INFO);
   exec(`git add ${projectName}`);
-  exec(`git commit -m '${ticketId} - ${projectName} release'`);
+  exec(`git commit -m "${ticketId} - ${projectName} release"`);
+  log(`switching to base branch ${baseBranch}`, INFO);
   exec(`git checkout ${baseBranch} && git pull origin ${baseBranch}`);
+  log(`merging ${branchName} into ${baseBranch}`, INFO);
+  exec(`git branch -d ${branchName}`);
   exec(`git merge ${branchName}`);
   exec(`git push origin ${baseBranch}`);
 };
