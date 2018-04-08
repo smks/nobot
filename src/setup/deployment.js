fc81176314e3ba3066ee31c8f854a35b567a66e3
Shaun Stone <shaunmstone@gmail.com>
2018-04-08 15:36:24 +0100

    resolve paths

--- src/setup/deployment.js
+++ src/setup/deployment.js
@@ -1,9 +1,9 @@
 const { cd, exec } = require('shelljs');
 const { existsSync } = require('fs');
-const { deploy: { name, repo } } = require('./../../config');
-const log = require('./../helpers/log');
-const repositoriesPath = require('./../helpers/get-repositories-path');
-const websitePath = require('./../helpers/get-website-path');
+const { deploy: { name, repo } } = require('../../config');
+const log = require('../helpers/log');
+const repositoriesPath = require('../helpers/get-repositories-path');
+const websitePath = require('../helpers/get-website-path');
 
 const setupDeployment = () => {
   if (existsSync(websitePath)) {
